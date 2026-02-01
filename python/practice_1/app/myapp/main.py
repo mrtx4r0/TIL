26c643e1a67970f1852fc4e709712b716c0fd391
mrtx4r0 <>
2026-02-01 10:55:14 +0900

    docsディレクトリに移動したものを削除。pythonの練習用ファイル追加

---
+++ python/practice_1/app/myapp/main.py
@@ -0,0 +1,18 @@
+import os
+print(f'os.getcwd():{os.getcwd()}')
+
+from class_sample import User
+
+user = User("tetsutaro", 31, "Tokyo, Japan")
+
+print(user.name)
+
+print(user.age)
+
+user.increment_age()
+print(user.age)
+
+user.start_name()
+
+
+
